f9cc4a40699efe890fe35c8e9d0f0b1e9189b94a
nguyentu13 <tunkph06861@fpt.edu.vn>
2020-02-13 12:00:15 +0700

    aaa

---
+++ GroupChatApplication/src/com/xtel/groupchat/Client.java
@@ -0,0 +1,71 @@
+package com.xtel.groupchat;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client implements Runnable {
+
+	Socket socketConnection;
+	DataOutputStream outToServer;
+	DataInputStream din;
+
+	Client() throws UnknownHostException, IOException {
+
+		socketConnection = new Socket("127.0.0.1", 8000);
+		outToServer = new DataOutputStream(socketConnection.getOutputStream());
+		din = new DataInputStream(socketConnection.getInputStream());
+
+		Thread thread;
+		thread = new Thread(this);
+		thread.start();
+
+		BufferedReader br = null;
+		String ClientName = null;
+		Scanner input = new Scanner(System.in);
+		String SQL = "";
+		try {
+			System.out.print("Enter you name: ");
+			ClientName = input.next();
+			ClientName += ": ";
+
+			br = new BufferedReader(new InputStreamReader(System.in));
+			while (!SQL.equalsIgnoreCase("exit")) {
+				System.out.println();
+				System.out.print(ClientName);
+
+				SQL = br.readLine();
+				// SQL = input.next();
+				outToServer.writeUTF(ClientName + SQL);
+//                            outToServer.flush();
+//                            System.out.println(din.readUTF());
+
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public static void main(String[] arg) throws UnknownHostException, IOException {
+		Client client = new Client();
+	}
+
+	public void run() {
+		while (true) {
+			try {
+				System.out.println("\n" + din.readUTF());
+
+			} catch (IOException e) {
+				e.printStackTrace();
+				break;
+			}
+
+		}
+	}
+}
